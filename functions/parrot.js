6854b965a5fbbd890757d8244a43a8dd63800a3f
AlexandreHosek <alexandre.hosek@gmail.com>
2020-11-10 14:57:15 +0100

    fonction parrot

--- functions/parrot.js
+++ functions/parrot.js
@@ -1,19 +1,19 @@
-exports.handler = async function(event, context) {
+exports.handler = async function (event, context) {
   const requestBody = JSON.parse(event.body);
   const message = requestBody.message;
 
   console.log(event);
 
-  if (event.httpMethod === "OPTIONS") {
+  if (event.httpMethod === 'OPTIONS') {
     const headers = {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Headers": "Content-Type",
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': 'Content-Type',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
     };
     return {
       statusCode: 200, // <-- Must be 200 otherwise pre-flight call fails
       headers,
-      body: "This was a preflight call!",
+      body: 'This was a preflight call!',
     };
   }
 
@@ -24,7 +24,7 @@ exports.handler = async function(event, context) {
     };
   }
   return {
-    statusCode: 400,
-    body: JSON.stringify({ message: "Nothing to repeat :(" }),
+    statusCode: 200,
+    body: JSON.stringify({ message: 'indiquez un message dans le body' }),
   };
 };
